994074f163c138a3663f6a61f90c4f4fb7f2ff41
ivanalekseevdotdot <ivanxalexeev@gmail.com>
2026-01-06 10:40:59 +0100

    class8 fix

--- class8_asad/perm_sup.py
+++ class8_asad/perm_sup.py
@@ -1,31 +1,26 @@
-# attention: code is wrong, i will fix it later
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 alpha = 0.9
 gamma = 2.0
 
-# regime 1: t = 0,1
+# regime 1 for t = 0,1
 y_bar1 = 5.0      
 pi_star = 4.0  # the inflation target remains unchanged
 
-# regime 2: t = 2
+# regime 2 for t = 2
 y_bar2 = 4.0      
 
-# shock: a permanent supply shock hits at t = 1
-s0 = 0.0      # t = 0: no shock
-s1 = 2.0      # t = 1: shock occurs (and is permanent)
-s2 = s1       # t = 2: shock persists
+# a permanent supply shock hits at t = 1
+s0 = 0.0      
+s1 = 2.0      
+s2 = s1       
 
 # ad function
-# (ad is independent of the supply shock.)
 def AD_func(y, y_bar, pi_star, z=0.0):
     return pi_star - (1/alpha)*(y - y_bar) + z/alpha
 
-# equilibrium solver test
-#   y = y_bar + (π★ - π_e - s) / (γ + 1/α)
-#   π = π★ - (1/α)(y - y_bar)
+# equilibrium solver
 def solve_equilibrium(pi_e_current, s, y_bar, pi_star):
     denom = gamma + 1/alpha
     num = pi_star - pi_e_current - s
@@ -33,19 +28,13 @@ def solve_equilibrium(pi_e_current, s, y_bar, pi_star):
     pi = pi_star - (1/alpha)*(y - y_bar)
     return y, pi
 
-# short-run as function test2
-# as is given by:
-#    as(y) = πₑ^(prev) + γ*(y - y_bar) + s
-# for t=0 we take πₑ^(prev) = π★.
+# short-run as function
 def AS_func(y, y_bar, pi_e_prev, s):
     return pi_e_prev + gamma*(y - y_bar) + s
 
-# simulate equilibrium for t = 0, 1, 2
-# t = 0: initial steady state (using regime 1)
+# solve equilibrium for t = 0, 1, 2
 y0, pi0 = solve_equilibrium(pi_e_current=pi_star, s=s0, y_bar=y_bar1, pi_star=pi_star)
-# t = 1: shock hits; ad remains unchanged (regime 1)
 y1, pi1 = solve_equilibrium(pi_e_current=pi0, s=s1, y_bar=y_bar1, pi_star=pi_star)
-# t = 2: central bank adjusts ad so that the new regime uses y_bar2
 y2, pi2 = solve_equilibrium(pi_e_current=pi1, s=s2, y_bar=y_bar2, pi_star=pi_star)
 
 # define grid for plotting curves
@@ -57,20 +46,14 @@ cmap = plt.get_cmap("tab10")
 plt.figure(figsize=(10,7))
 
 # plot ad curves
-# regime 1 ad (t = 0,1)
 AD_regime1 = [AD_func(y, y_bar1, pi_star) for y in y_grid]
 plt.plot(y_grid, AD_regime1, '-', color=cmap(0), linewidth=2, label=r'$AD_{0}=AD_{1}$')
-# regime 2 ad (t = 2)
 AD_regime2 = [AD_func(y, y_bar2, pi_star) for y in y_grid]
 plt.plot(y_grid, AD_regime2, '-', color=cmap(1), linewidth=2, label=r'$AD_{2}$')
-
-# compute short-run as curves for each period
 AS0 = [AS_func(y, y_bar1, pi_star, s0) for y in y_grid]
 AS1 = [AS_func(y, y_bar1, pi0, s1) for y in y_grid]
-# for period 2 use π★ as the base and then shift by (pi1 - pi_star)
 AS2 = [AS_func(y, y_bar1, pi_star, s2) + (pi1 - pi_star) for y in y_grid]
 
-# group as curves if they are identical (within tolerance)
 AS_curves = [AS0, AS1, AS2]
 periods = [0, 1, 2]
 tol_val = 1e-3
@@ -108,8 +91,6 @@ plt.plot(y1, pi1, 'ko', markersize=8, label='_nolegend_')
 denom = gamma + 1/alpha
 y_intersect = (pi_star - pi1 - s2 + gamma*y_bar1 + (1/alpha)*y_bar2) / denom
 pi_intersect = pi_star - (1/alpha) * (y_intersect - y_bar2)
-
-# plot the intersection point for period 2 (black dot, no legend)
 plt.plot(y_intersect, pi_intersect, 'ko', markersize=8, label='_nolegend_')
 
 plt.xlabel('$y$')
